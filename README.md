c94ae18116f10cfcb03e0e162fffd57017204a5a
Jacob Peterson <petersonjacobw@gmail.com>
2021-02-15 17:49:04 -0800

    Working files for Tic-Tac-Toe with styling for testing functionality

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a basic Tic-Tac-Toe implementation using vanilla Javascript without help from external libraries.
+
+Game Play:
+1) Click a square to place your piece
+2) A winner is declared when three X-pieces or Y-pieces are connected horizontally, vertically, or diagonally.
+3) The board can be reset with the "reset" button
---
+++ app.js
@@ -0,0 +1,121 @@
+const squares = document.getElementsByTagName("td");
+const result = document.getElementsByClassName("title");
+const tableRows = document.getElementById("gameBoard").rows;
+
+/*
+
+Adds event listeners on squares and handles the actions of assigning a letter to that square
+
+*/
+
+//Adds listener to each square
+for (const square of squares) {
+  square.addEventListener("click", squareClickHandler);
+}
+
+//Handles click on square, delegating letter assignment to helper function and calling helper function to check for winner
+function squareClickHandler(event) {
+  event.target.innerHTML = nextLetter(lastLetter);
+  checkWin(lastLetter);
+  checkFullBoard();
+}
+
+//variable and helper function for assigning alternating Xs and Os
+var lastLetter = 'O';
+function nextLetter(letter) {
+  if (letter === 'X') {
+    lastLetter = 'O';
+    return 'O';
+  } else {
+    lastLetter = 'X';
+    return 'X';
+  }
+}
+
+
+/*
+
+Adds variable for reset button and event listeners and handlers for the button
+
+*/
+const button = document.getElementById("reset");
+
+button.addEventListener('click', resetClickHandler);
+
+function resetClickHandler() {
+  for (const square of squares) {
+    square.innerHTML = '';
+  }
+}
+
+/*
+
+Function and helpers for checking for a winner
+
+*/
+
+//checks all three winning options
+function checkWin(piece) {
+  if (rowCheck(piece) || columnCheck(piece) || diagonalLine(piece)) {
+    window.alert(`${piece} is the Winner`);
+  }
+}
+
+//checks for a row win
+function rowCheck(piece) {
+  var connectedRow = false;
+  for (var i = 1; i < tableRows.length; i++) {
+    var matchedPieces = 0
+    for (var j = 0; j < tableRows[i].cells.length; j++) {
+      if (tableRows[i].cells[j].innerHTML === piece) {
+        matchedPieces++;
+      }
+    }
+    if (matchedPieces === 3) {
+      connectedRow = true;
+    }
+  }
+  return connectedRow;
+}
+
+//checks for a column win
+function columnCheck(piece) {
+  var connectedLine = false;
+  for (var i = 0; i < tableRows[1].cells.length; i++) {
+    var matchedPieces = 0;
+    for (var j = 1; j < tableRows.length; j++) {
+      if (tableRows[j].cells[i].innerHTML === piece) {
+        matchedPieces++;
+      }
+    }
+    if (matchedPieces === 3) {
+      connectedLine = true;
+    }
+  }
+  return connectedLine;
+}
+
+//checks for a diagonal win running from top-left to bottom-right and from bottom-left to top-right
+function diagonalLine(piece) {
+  var hasDiagonalLine = false;
+  if ((tableRows[1].cells[0].innerHTML === piece
+    && tableRows[2].cells[1].innerHTML === piece
+    && tableRows[3].cells[2].innerHTML === piece)
+    ||
+    (tableRows[1].cells[2].innerHTML === piece
+      && tableRows[2].cells[1].innerHTML === piece
+      && tableRows[3].cells[0].innerHTML === piece)) {
+      hasDiagonalLine = true
+    }
+
+  return hasDiagonalLine
+}
+
+/*
+
+Function and helpers for checking for a full board (i.e. no winner)
+
+*/
+function checkFullBoard() {
+
+}
